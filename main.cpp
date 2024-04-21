6279e26643beae2a2ca0c7fe098c161c9409bc6b
nytouu <capitaine7312@gmail.com>
2024-04-21 19:28:34 +0200

    rotate around box

--- main.cpp
+++ main.cpp
@@ -212,8 +212,11 @@ class Application
 
             shader.use();
 
-            glm::mat4 view = glm::mat4(1.0f);
-            view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+            const float radius = 10.0f;
+            float camX = sin(glfwGetTime()) * radius;
+            float camZ = cos(glfwGetTime()) * radius;
+            glm::mat4 view;
+            view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
 
             glm::mat4 projection;
             projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
